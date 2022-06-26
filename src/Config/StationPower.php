81505434debf656d4ae185dbbb8cd94b71246666
Antoine Dumez <MY_NAME@example.com>
2022-06-26 20:21:54 +0200

    conflit_fix

--- src/Config/StationPower.php
+++ src/Config/StationPower.php
@@ -4,10 +4,10 @@ namespace App\Config;
 
 enum StationPower: int
 {
-    case kW3 = 2;
-    case kW7 = 7;
-    case kW18 = 18;
-    case kW43 = 43;
-    case kW100 = 100;
-    case kW350 = 350;
+case kW3 = 2;
+case kW7 = 7;
+case kW18 = 18;
+case kW43 = 43;
+case kW100 = 100;
+case kW350 = 350;
 }

e55927ce79356e77e46e87fd933efcbb54a3f31a
Antoine Dumez <MY_NAME@example.com>
2022-06-26 20:28:04 +0200

    conflit_fix

--- src/Config/PlugType.php
+++ src/Config/PlugType.php
@@ -4,10 +4,10 @@ namespace App\Config;
 
 enum PlugType: string
 {
-case Domestic = 'domestic';
-case Type1 = 'type1';
-case Type2 = 'type2';
-case Type3c = 'type3c';
-case Type2attached = 'type2attached';
-case ComboCSS = 'comboCSS';
+    case Domestic = 'domestic';
+    case Type1 = 'type1';
+    case Type2 = 'type2';
+    case Type3c = 'type3c';
+    case Type2attached = 'type2attached';
+    case ComboCSS = 'comboCSS';
 }

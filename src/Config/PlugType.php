f4922b8847fafd4381802b61ad5f1f6c4c6f3308
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-07-05 08:34:37 +0200

    wip

--- src/Config/PlugType.php
+++ src/Config/PlugType.php
@@ -4,9 +4,9 @@ namespace App\Config;
 
 enum PlugType: string
 {
-case Domestic = 'domestic';
-case Type1 = 'type1';
-case Type2 = 'type2';
-case Type3c = 'type3c';
-case Type2attached = 'type2attached';
-    }
+    case Domestic = 'domestic';
+    case Type1 = 'type1';
+    case Type2 = 'type2';
+    case Type3c = 'type3c';
+    case Type2attached = 'type2attached';
+}

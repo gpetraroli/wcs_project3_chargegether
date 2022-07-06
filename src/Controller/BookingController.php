1021676969a61b55c78e4db9750fc4f8bd49fa0c
Antoine Dumez <MY_NAME@example.com>
2022-07-06 11:58:48 +0200

    controller_booking_add

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Booking;
+use App\Entity\Station;
+use App\Form\BookingType;
+use App\Repository\BookingsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,16 +18,49 @@ class BookingController extends AbstractController
     #[Route('/reservations', name: 'booking')]
     public function showBookingsList(Request $request, EntityManagerInterface $manager): Response
     {
-        $booking = new Booking();
-
         return $this->render('booking/index.html.twig');
     }
 
     //nouvelle reservation
-    #[Route('/reservation/new', name: 'add_booking')]
-    public function addBooking(): Response
+    #[Route('/hote/reserver/{id}', name: 'add_booking')]
+    public function addBooking(Station $station, Request $request, BookingsRepository $bookingsRepository): Response
     {
-        return $this->render('booking/addbooking.html.twig');
+        $fees = 1;
+        $coefficient = 2.5;
+        $electricityPrice = 0.3;
+
+        $booking = New Booking();
+        $booking->setStation($station);
+        $booking->setUser($this->getUser());
+
+        $form = $this->createForm(BookingType::class , $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $booking->setVehicle($form->get('vehicle')->getData());
+            $booking->setStartRes($form->get('startRes')->getData());
+            $booking->setEndRes($form->get('endRes')->getData());
+            // calcul du prix de la réservation
+            $interval = $form->get('endRes')->getData()->diff($form->get('startRes')->getData());
+            $intervarInHours = $interval->format('%h') + $interval->format('%i')/60;
+            $vehiclePower = $form->get('vehicle')->getData()->getBatteryPower();
+            $stationPower = $station->getPower()->value;
+            $power = ($vehiclePower < $stationPower) ? $vehiclePower : $stationPower;
+            $price = $electricityPrice * $coefficient * $intervarInHours * $power + $fees;
+
+            $booking->setBookingPrice(strval($price));
+            $bookingsRepository->add($booking, true);
+            $this->addFlash('success', 'nouvelle résa effectuée');
+
+            return $this->redirectToRoute('booking');
+
+        }
+
+
+
+        return $this->render('booking/addbooking.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     //recapitulatif infos resa

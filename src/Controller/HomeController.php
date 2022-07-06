0c434ed2a30ee79d2947926fbec0b82e430d1064
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-07-06 16:43:02 +0200

    pr-ready

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\NotificationsRepository;
-use App\Repository\StationsRepository;
-use App\Service\NotificationManager;
+use App\Service\VehicleManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(VehicleManager $vehicleManager): Response
     {
         return $this->render('home/index.html.twig', [
             'googleApiKey' => $this->getParameter('google_api_key'),
+            'selectedVehicleId' => $vehicleManager->getSelectedVehicle(),
         ]);
     }
 }

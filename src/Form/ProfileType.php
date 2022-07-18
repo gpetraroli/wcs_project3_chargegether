834923bfa8a0fbe8a3d4bbdfd35d70a93704e25a
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-07-18 12:08:09 +0000

    fixed

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 
 class ProfileType extends AbstractType
